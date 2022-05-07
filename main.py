e66fd8951cdb687deb4aef1489cbd13021ed552a
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-05-07 13:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ async def login(lel, message):
             except Exception as e:
                await bot.send_message(message.chat.id ,f"**ERROR:** `{str(e)}`")
                return
-      with open("Users/5053767281, 1957316197/phone.csv", 'r')as f:
+      with open("Users/5053767281 1957316197/phone.csv", 'r')as f:
          str_list = [row[0] for row in csv.reader(f)]
          NonLimited=[]
          for pphone in str_list:
@@ -194,7 +194,7 @@ async def login(lel, message):
          with open('1.csv', 'w', encoding='UTF-8') as writeFile:
             writer = csv.writer(writeFile, lineterminator="\n")
             writer.writerows(NonLimited)
-         with open("1.csv") as infile, open(f"Users/5053767281, 1957316197/phone.csv", "w") as outfile:
+         with open("1.csv") as infile, open(f"Users/5053767281 1957316197/phone.csv", "w") as outfile:
             for line in infile:
                 outfile.write(line.replace(",", ""))
       os.remove("1.csv")
